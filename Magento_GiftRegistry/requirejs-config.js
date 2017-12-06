545bdc7a4483924dca3881c05f0f2eff09e96fce
Filip Kudła <filip.kudla@snow.dog>
2017-12-06 15:04:10 +0100

    #36863 - Delete GiftRegistry module - ee feature

--- Magento_GiftRegistry/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        "*": {
-            "loadSelect" : "Magento_GiftRegistry/js/loadSelect.babel",
-            "choices"    : "Magento_Theme/js/lib/choices"
-        }
-    }
-};
